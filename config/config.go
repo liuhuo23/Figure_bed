3b95c1d7195e6648a259aabbaefcccbb1e4275b2
liuhuo <zhaoyj2370@163.com>
2022-10-18 22:04:12 +0800

    解决一些bug

--- config/config.go
+++ config/config.go
@@ -1,19 +1,13 @@
 package config
 
-import (
-	"log"
-
-	"gopkg.in/ini.v1"
-)
-
 type Config struct {
 	Port     int    `ini:"port"`      // 服务器端口
 	SavePath string `ini:"save_path"` // 保存路径
 }
 
 func NewInit() {
-	cfg, err := ini.Load("./figure.ini")
-	if err != nil {
-		
-	}
+	// cfg, err := ini.Load("./figure.ini")
+	// if err != nil {
+
+	// }
 }
